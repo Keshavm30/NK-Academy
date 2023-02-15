c855c166ff0b63e2869fdf95a99b25931667dc09
Keshavm30 <59768236+Keshavm30@users.noreply.github.com>
2023-02-15 22:55:20 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<script>
+window.location.href = "https://keshavm30.github.io/NK-Academy/<?php echo $_POST["PNO"]; ?>.html";
+
+</script>
+
+<a href="https://keshavm30.github.io/NK-Academy/<?php echo $_POST["PNO"]; ?>.html"> go to page</a>
+
+
+
+
+
+
+</body>
+</html>
